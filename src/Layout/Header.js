4ed1f55b05590528eda507afbd0b33c609629a08
faiz033033 <feju302@gmail.com>
2023-08-22 22:03:31 +0530

    LogoutAdded

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,15 +1,39 @@
-import React from "react";
-import classes from './Header.css'
-const Header=()=>
-{
-    return(<React.Fragment>
-        <header className={classes.header}>
-            <ul>
-                <li><p>Home</p></li>
-                <li><p>Product</p></li>
-                <li><p>About Us</p></li>
-            </ul>
-        </header>
-    </React.Fragment>)
-}
+import React, { useContext } from "react";
+import classes from "./Header.css"; 
+import ExpenseContext from "../store/expense-context";
+import { useHistory } from "react-router-dom";
+
+const Header = () => {
+  const expctx = useContext(ExpenseContext);
+  const history = useHistory();
+
+  const userlogOuthandler = () => {
+    expctx.logOut();
+    history.replace("/");
+  };
+
+  return (
+    <header className={classes.header}>
+      <ul>
+        <li>
+          <p>Home</p>
+        </li>
+        <li>
+          <p>Product</p>
+        </li>
+        <li>
+          {expctx.ExpenseToken && (
+            <button
+              onClick={userlogOuthandler}
+              className={classes.logout_button}
+            >
+              Log Out
+            </button>
+          )}
+        </li>
+      </ul>
+    </header>
+  );
+};
+
 export default Header;
